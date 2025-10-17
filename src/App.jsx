44796f44f8569612acdeaf581d24f60d634084b8
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-17 15:31:44 +0600

    Explore JSX and Props basics

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>React Core Concept</h1>
       <Person></Person>
+      <Student></Student>
+      <Developer name="Tafsir" tech="JS"></Developer>
+      <Developer name="Sagor" tech="python"></Developer>
+      <Developer name="Sakib" tech="C#"></Developer>
+      <Device name="Laptop" price="50000"></Device>
+      <Device name="Mobile" price="35000"></Device>
+      <Device name="Watch" price="2000"></Device>
+
+      {/* <Person></Person>
       <MyPet></MyPet>
       <Person></Person>
       <MyPet1></MyPet1>
@@ -15,7 +24,7 @@ function App() {
       <MyPet2></MyPet2>
 
       <Sports></Sports>
-      <Sports2></Sports2>
+      <Sports2></Sports2> */}
 
     </>
   )
@@ -24,8 +33,52 @@ function App() {
 function Person() {
   const name = "John Doe";
   const age = 30;
+
+  const personStyle = {
+    color: "red",
+    backgroundColor: "yellow",
+    textAlign: "center",
+    margin: "20px",
+    padding: "20px",
+  }
+
   return (
-    <p>I am {name}. I am {age} years old </p>
+    <p style={personStyle} >I am {name}. I am {age} years old </p>
+  )
+}
+
+function Developer(props) {
+  console.log(props);
+  return (
+  <div style={{
+        border: "2px solid saffron",
+        borderRadius: "20px",
+      }}>
+    <h4>Developer: {props.name} </h4>
+    <p>Technology: {props.tech} </p>
+  </div>
+  )
+}
+
+function Device(props) {
+  return(
+    <div style={{
+      border: "2px solid blue",
+      borderRadius: "20px",
+      margin: "20px",
+    }}>
+      <h2>Device: {props.name} </h2>
+      <p>Price: {props.price} </p>
+    </div>
+  )
+}
+
+function Student() {
+  return (
+    <div className='student'>
+      <p>Name: </p>
+      <p>Dept: </p>
+    </div>
   )
 }
 
