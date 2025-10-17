1a171c92257bf275fe906038dcae95607a3b283b
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-17 23:44:27 +0600

    Rendering: If, Ternary and AND OR

--- src/todo.jsx
+++ src/todo.jsx
@@ -21,12 +21,56 @@
 //    }
 // }
 
-export default function ToDo({task, isDone, time="100"}) {
-    if(isDone) {
-        return <li>Done: {task} {time} min✅ </li>
-    }
-    // else {
-    //     return <li>Pending: {task}⏳ </li>
-    // }
-    return <li>Pending: {task}⏳ </li>
+// export default function ToDo({task, isDone, time="100"}) {
+//     if(isDone) {
+//         return <li>Done: {task} {time} min✅ </li>
+//     }
+//     // else {
+//     //     return <li>Pending: {task}⏳ </li>
+//     // }
+//     return <li>Pending: {task}⏳ </li>
+// }
+
+// conditional rendering: 3 ternary
+// condition ? true : false
+
+// export default function ToDo({task, isDone, time=0}) {
+//     return isDone ? <li>Done: {task} time: {time} </li> : <li>Not Done: {task} </li>
+    
+// }
+
+// conditional rendering: 4 &&
+// export default function ToDo({task, isDone, time=0}) {
+//     return isDone && <li>Done Task: {task} time: {time} </li>
+// }
+
+// conditional rendering: 5 ||
+// export default function ToDo({task, isDone, time=0}) {
+//     return isDone || <li> Not Done Task: {task} time: {time} </li>
+// }
+
+// export default function ToDo({task, isDone}) {
+//    if (isDone) {
+//     return null
+//    }
+//    else {
+//     return <li>{task}</li>
+//    }
+// }
+
+
+// conditional rendering: 6 variable
+export default function ToDo({task, isDone, time}) {
+
+    const displayTime = time ? time : 70;
+    let listItem;
+
+   if (isDone) {
+    listItem = <li>Task: {task} <br/> Time: {displayTime} ✅</li>
+   }
+   else {
+    listItem = <li>{task} ❌</li>
+   }
+
+   return listItem
 }
